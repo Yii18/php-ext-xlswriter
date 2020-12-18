bdacb2326d2c9c446f294447f51f750155d1b47d
Yii18 <34769669+Yii18@users.noreply.github.com>
2020-12-18 17:54:49 +0800

    Update excel.php

--- excel.php
+++ excel.php
@@ -15,5 +15,22 @@ $setData = $setHeader->data([
 var_dump($setData);
 $output = $setData->output();
 var_dump($output);
+
+
+
+# 出现没有过滤空行代码实例
+$config   = ['path' => './tests/'];
+$excel    = new \Vtiful\Kernel\Excel($config);
+$data = $excel->openFile('1.xlsx')
+    ->sheetList();
+$result = [];
+foreach ($data as $sheetName){
+    $rows = $excel->openSheet($sheetName,\Vtiful\Kernel\Excel::SKIP_EMPTY_CELLS | \Vtiful\Kernel\Excel::SKIP_EMPTY_VALUE | \Vtiful\Kernel\Excel::SKIP_EMPTY_ROW);
+    while (($row = $rows->nextRow()) !== NULL){
+       var_dump($row);
+    }
+    var_dump($rows->getSheetData());
+}
+
 ?>
 
